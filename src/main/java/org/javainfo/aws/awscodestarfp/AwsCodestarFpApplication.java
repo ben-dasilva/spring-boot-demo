9fd6f2821b408c4f959482f431fd216c81c4517c
Ben da Silva <d123097@gmail.com>
2017-09-30 15:03:35 -0400

    first commit

---
+++ src/main/java/org/javainfo/aws/awscodestarfp/AwsCodestarFpApplication.java
@@ -0,0 +1,12 @@
+package org.javainfo.aws.awscodestarfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodestarFpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodestarFpApplication.class, args);
+	}
+}
